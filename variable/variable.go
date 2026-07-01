7e09c32dc162997d0927481e2bdc064174ae364c
Sainzel <cppnaf@gmail.com>
2026-07-01 17:57:52 +0700

    Go Variable

---
+++ variable/variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "andi"
+	var umur int = 18
+
+	fmt.Println("nama kamu : ", name)
+	fmt.Println("umur kamu : ", umur)
+
+	// Go juga bisa langusng memprediksi apa tipe data nya
+
+	Bulan := "september"
+	Tahun := 2000
+
+	fmt.Println("lahir bulan : ", Bulan)
+	fmt.Println("pada tahun : ", Tahun)
+
+}
