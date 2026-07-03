8c0bc9bcb17cc83c584cf3b19cb8a593c95907a1
Sainzel <cppnaf@gmail.com>
2026-07-03 17:58:00 +0700

    with inferred lengths

--- array/array.go
+++ array/array.go
@@ -5,7 +5,12 @@ import "fmt"
 func main () {
 	var x = [3]int{1,2,3}
 	y := [5]int{4,5,6,7,8}
+	var arr1 = []int{1,2,3}
+	arr2 := []int{4,5,6,7,8}
 
 	fmt.Println(x)
 	fmt.Println(y)
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
 }
