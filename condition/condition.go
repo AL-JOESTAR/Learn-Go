f17a8e96c85e32de37e4e16fd2bef886efa50184
Sainzel <cppnaf@gmail.com>
2026-07-05 20:49:44 +0700

    condition

---
+++ condition/condition.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 20 > 18{
+		fmt.Println("20 lebih besar dari 18")
+	}
+
+	x := 10
+	y := 20
+	if x < y {
+		fmt.Println("x lebih kecil dari y")
+	}
+}
