10f70e1ec1958e37a65709ec1cc9e9979f539004
Sainzel <cppnaf@gmail.com>
2026-07-11 21:16:05 +0700

    Switch

--- switch/Switch.go
+++ switch/Switch.go
@@ -20,4 +20,38 @@ func main() {
   case 7:
     fmt.Println("Sunday")
   }
+
+  week := 8
+
+  switch week {
+  case 1:
+    fmt.Println("Monday")
+  case 2:
+    fmt.Println("Tuesday")
+  case 3:
+    fmt.Println("Wednesday")
+  case 4:
+    fmt.Println("Thursday")
+  case 5:
+    fmt.Println("Friday")
+  case 6:
+    fmt.Println("Saturday")
+  case 7:
+    fmt.Println("Sunday")
+  default:
+    fmt.Println("Not a weekday")
+  }
+
+  day_week := 5
+
+   switch day_week {
+   case 1,3,5:
+    fmt.Println("Odd weekday")
+   case 2,4:
+     fmt.Println("Even weekday")
+   case 6,7:
+    fmt.Println("Weekend")
+  default:
+    fmt.Println("Invalid day of day number")
+  }
 }
