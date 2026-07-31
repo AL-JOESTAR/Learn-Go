2e25c86dead022a994fd3112b6ad505bac74b16e
Sainzel <cppnaf@gmail.com>
2026-07-31 21:02:26 +0700

    function dengan parameter

--- function/function.go
+++ function/function.go
@@ -6,8 +6,15 @@ func myMessage(){
 	fmt.Println("hai ini function")
 }
 
+func nama(fname string){
+	fmt.Println("hello", fname , "kanedy")
+}
+
 func main(){
 	myMessage()
 	myMessage()
 	myMessage()
+
+	nama("john")
+	nama("robert")
 }
